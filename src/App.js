d6b4049e85fa53e4397c9d0a991b6ac4647e4965
JorgeJ <akiubarx@gmail.com>
2022-06-16 11:35:55 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import  Acceso  from './components/acceso.jsx';
 import  NaviBar  from './components/naviBar.jsx';
-import  ButtonB  from './components/buttons.js';
 import  './css/style.css';
-import { Button } from 'react-bootstrap';
-import Carpetas from "./pages/carpetas";
 import {Outlet} from "react-router-dom";
 
 
--- src/pages/carpetas.jsx
+++ src/pages/carpetas.jsx
@@ -1,58 +1,58 @@
 import React from 'react';
-import { Container, Col, Row, Table, Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import { Container, Table, Navbar, Form, FormControl, Button } from 'react-bootstrap';
 import NaviBar from '../components/naviBar.jsx';
 
 const Carpetas = () =>{
   return(
-    <div>
-    <NaviBar></NaviBar>
-    <h1>Carpetas</h1>
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Form className="d-flex">
-            <FormControl
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-    </div>
+    <>
+      <NaviBar></NaviBar>
+      <h1>Carpetas</h1>
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
   )
 }
 
