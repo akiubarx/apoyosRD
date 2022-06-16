3e9162a1bf25d73cc0dbf4c39f65d24d9e5dd662
JorgeJ <akiubarx@gmail.com>
2022-06-16 12:41:01 -0500

    cambio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import  Acceso  from './components/acceso.jsx';
 import  NaviBar  from './components/naviBar.jsx';
 import  './css/style.css';
-import {Outlet} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
       <NaviBar></NaviBar>
-      <Outlet></Outlet>
       <Acceso></Acceso>
     </div>
   );
--- src/components/naviBar.jsx
+++ src/components/naviBar.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 import {Container, Navbar, Nav} from 'react-bootstrap';
-import { Outlet } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const NaviBar = () =>{
   return(
-    <Navbar bg="light" variant="light">
-      <Container>
-      <Navbar.Brand href="#home">
-        <img
-          alt=""
-          src="/logo.svg"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
-        Sistema Apoyos
-      </Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="/carpetas">Carpetas</Nav.Link>
-        <Nav.Link href="#archivos">Archivos</Nav.Link>
-        <Nav.Link href="#subir_archivos">Subir Archivos</Nav.Link>
-        <Nav.Link href="#apoyos">Apoyos</Nav.Link>
-        <Nav.Link href="#usuarios">Usuarios</Nav.Link>
-        <Nav.Link href="#permisos">Permisos</Nav.Link>
-        <Nav.Link href="#logout">Salir</Nav.Link>
-      </Nav>
-      </Container>
-    </Navbar>
+    <BrowserRouter>
+      <Navbar bg="light" variant="light">
+        <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          Sistema Apoyos
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link className='nav-link' to='/carpetas'>Carpetas</Nav.Link>
+          <Nav.Link href="#archivos">Archivos</Nav.Link>
+          <Nav.Link href="#subir_archivos">Subir Archivos</Nav.Link>
+          <Nav.Link href="#apoyos">Apoyos</Nav.Link>
+          <Nav.Link href="#usuarios">Usuarios</Nav.Link>
+          <Nav.Link href="#permisos">Permisos</Nav.Link>
+          <Nav.Link href="#logout">Salir</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+    </BrowserRouter>
   )
 }
 
